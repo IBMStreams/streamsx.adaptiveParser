b78f318b7f01c6b8921b3228dbdeedaab59ad8d0
leongor <leongor@gmail.com>
2015-04-19 15:41:49 +0300

    Changed internal implementation of formatted timestamp.

--- com.ibm.streamsx.adaptiveparser/com.ibm.streamsx.adaptiveparser/AdaptiveParser/AdaptiveParser_h.pm
+++ com.ibm.streamsx.adaptiveparser/com.ibm.streamsx.adaptiveparser/AdaptiveParser/AdaptiveParser_h.pm
@@ -136,7 +136,6 @@ sub main::generate($$) {
    print '//		timestampS = skip(blank)[eps] >> ("(" >> long_ >> "," >> uint_ >> "," >> uint_ >> ")") [_val = construct<SPL::timestamp>(_1, _2, _3)];', "\n";
    print '		timestamp = skip(blank)[eps] >> long_[bind(&SPL::timestamp::setSeconds,_val,_1)] >> lit(_r1) >> uint_[bind(&SPL::timestamp::setNanoSeconds,_val,_1)];', "\n";
    print '		timestampS = skip(blank)[eps] >> "(" >> long_[bind(&SPL::timestamp::setSeconds,_val,_1)] >> "," >> uint_[bind(&SPL::timestamp::setNanoSeconds,_val,_1)] >> "," >> int_[bind(&SPL::timestamp::setMachineId,_val,_1)] >> ")";', "\n";
-   print '//		timestampF = (skip(blank)[eps] >> STR_(,)) [_val = bind(&TupleParserGrammar<charPtr>::parseTS, _r1, construct<std::string>(bind(&iterator_range<charPtr>::begin,_1), bind(&iterator_range<charPtr>::end,_1)))];', "\n";
    print "\n";
    print '    	';
    print $baseRule;
@@ -158,7 +157,6 @@ sub main::generate($$) {
    print "\n";
    print '	qi::rule<Iterator,  SPL::timestamp(std::string)> timestamp;', "\n";
    print '	qi::rule<Iterator,  SPL::timestamp()> timestampS;', "\n";
-   print '	qi::rule<Iterator,  SPL::timestamp(std::string)> timestampF;', "\n";
    print "\n";
    print '    qi::rule<Iterator, oport0(bool&)> oport0_base;', "\n";
    print '    ', "\n";
